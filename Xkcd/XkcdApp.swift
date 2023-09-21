3658c6fb15acece85a202f20fbc2da4add2b7657
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-09-21 13:27:59 -0600

    Initial Commit

---
+++ Xkcd/XkcdApp.swift
@@ -0,0 +1,17 @@
+//
+//  XkcdApp.swift
+//  Xkcd
+//
+//  Created by Rajeev R Menon on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct XkcdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
